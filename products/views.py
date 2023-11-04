901068a02b087e20ce8e8da3e39095a54e541500
sarahliz24 <sarahliz24@hotmail.com>
2023-11-04 13:14:44 +0000

    Add: add_ product view, template & form

--- products/views.py
+++ products/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category
+from .forms import ProductForm
 
 def all_products(request):
     '''
@@ -70,6 +72,17 @@ def product_detail(request, artwork_id):
         'product': product,
     }
 
-
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add artwork to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
 
