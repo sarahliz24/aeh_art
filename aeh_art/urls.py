d5a17aa1ec94674f87cc904f08c86f782988b358
sarahliz24 <sarahliz24@hotmail.com>
2023-11-05 17:11:23 +0000

    Add: 500 error handler

--- aeh_art/urls.py
+++ aeh_art/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+from .views import handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +30,4 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'aeh_art.views.handler404'
+handler500 = 'aeh_art.views.handler500'
