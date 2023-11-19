bc255b4f09d75a1839b72872d3caea0e2eea1164
sarahliz24 <sarahliz24@hotmail.com>
2023-11-19 19:16:26 +0000

    Add: links & htmk structure for bio pages

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.all_products, name='products'),
     path('about/', views.about, name='about'),
+    path('artist/', views.artist, name='artist'),
+    path('scientist/', views.scientist, name='scientist'),
+    path('human/', views.human, name='human'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,3 +6,24 @@ def about(request):
     A view to return the about page
     '''
     return render(request, 'pages/about.html')
+
+
+def artist(request):
+    '''
+    A view to return the artist page
+    '''
+    return render(request, 'pages/artist.html')
+
+
+def scientist(request):
+    '''
+    A view to return the acientist page
+    '''
+    return render(request, 'pages/cientist.html')
+
+
+def human(request):
+    '''
+    A view to return the human page
+    '''
+    return render(request, 'pages/human.html')
