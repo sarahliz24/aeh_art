2f9a4a8b2ad714a79ad154e00688da43101369aa
sarahliz24 <sarahliz24@hotmail.com>
2023-11-25 13:01:54 +0000

    Update: model for date ordering

--- publications/models.py
+++ publications/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 class Publications(models.Model):
 
+    class Meta:
+        ordering = ('-publication_year',)
+        verbose_name = 'Publications'
+        verbose_name_plural = 'Publications'
+
     title = models.CharField(max_length=150)
     authors = models.CharField(max_length=500)
     publication_year = models.IntegerField()
@@ -11,7 +16,6 @@ class Publications(models.Model):
     page_range = models.CharField(max_length=500, null=True, blank=True, default="n/a")
     doi = models.CharField(max_length=150, null=True, blank=True)
     link_url = models.URLField(max_length=1024, null=True, blank=True)
-    
 
     def __str__(self):
         return self.title
