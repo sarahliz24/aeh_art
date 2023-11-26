a7f0f87fd7e688039ed82d3028b165ff8b8a55d5
sarahliz24 <sarahliz24@hotmail.com>
2023-11-26 17:12:06 +0000

    Update: newletter model with ordering by date

--- newsletter/models.py
+++ newsletter/models.py
@@ -2,6 +2,14 @@ from django.db import models
 
 
 class Newsletter(models.Model):
+    '''
+    Model for newsletter
+    '''
+
+    class Meta:
+        ordering = ('-date',)
+
+
     title = models.CharField(max_length=150)
     date = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
