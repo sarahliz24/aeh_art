0c924393e93e804619cee1fb49911cf5b3437358
sarahliz24 <sarahliz24@hotmail.com>
2023-11-27 10:56:05 +0000

    Fix: line too tong flake8

--- bag/views.py
+++ bag/views.py
@@ -33,7 +33,10 @@ def add_to_bag(request, item_id):
 
 
 def adjust_bag(request, item_id):
-    """ Adjust the quantity of the specified product to the amount the user indicates """
+    """
+    Adjust the quantity of the specified product
+    to the amount the user indicates
+    """
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
