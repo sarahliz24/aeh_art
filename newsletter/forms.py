4879ed01b4528972371d477892234727ae315745
sarahliz24 <sarahliz24@hotmail.com>
2023-11-27 10:55:31 +0000

    Fix: remove unused code from form

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -7,7 +7,3 @@ class NewsletterForm(forms.ModelForm):
     class Meta:
         model = Newsletter
         fields = '__all__'
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        newlsetter = Newsletter.objects.all()
