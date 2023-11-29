655ae7c0026fa419be0522ca27ceb8018b5d370e
sarahliz24 <sarahliz24@hotmail.com>
2023-11-29 13:50:30 +0000

    Update: add noqa to required import statements

--- aeh_art/settings.py
+++ aeh_art/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 from pathlib import Path
 
 if os.path.exists("env.py"):
-    import env
+    import env  # noqa
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
